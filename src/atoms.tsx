fac82627abd67181132bb6fd2d8c47584bc63929
dongmyoungLee <pajang1515@daum.net>
2022-02-20 23:31:23 +0900

    end

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,12 +1,40 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+
+
+// 자주쓰는값을 저장함, 좌변만 넣으면 index순서에따라 0,1,2,3... 의 값을 같게됨.
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE"
+}
 
 export interface IToDo {
   text : string
   id : number,
-  category : "TO_DO" | "DOING" | "DONE"
+  category : Categories
 }
 
+export const categoryState = atom<Categories>({
+  key : "category",
+  default : Categories.TO_DO
+})
+
+// 1. 새로 추가하는 toDo들이 모두 toDoState로 들어가서 보여줌 -> 하지만 toDO를 보려고할때는 selector 라는걸 이용했음. selector 는 state를 가져다가 조금 변형해주는 함수임.
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+})
+
+// 2. get function 은 selector 가 어떤것을 반환할지 결정한다. 이 selector 는 두개의 atom 을 이용하고 있음. 모든 toDo를 저장하고있는 toDos 와 category의 상태를 저장하는 atorm
+
+// 3. todoList 에서 select 태그를 만들어주고 input event 를 감지해서 선택에따라 category 의 상태를 바꿔주고 있음. 이건 즉 selector 가 변하고 있다는 뜻임. categoryState 가 변할때마다 selector도 같이 실행됨.
+
+// 4. 이 selector는 toDo들을 가져다 categoryState에 맞는 toDo만을 걸러서 반환해줌.
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({get}) => {
+    const toDos = get(toDoState) // 들어와있는 배열
+    const category = get(categoryState) // select 태그의 onInput함수로 인해 변경되고있음.
+    return toDos.filter((toDo) => toDo.category === category)
+  }
 })
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,62 @@
-import { IToDo } from "../atoms"
+import { useSetRecoilState } from "recoil"
+import { Categories, IToDo, toDoState } from "../atoms"
 
-function ToDo({text} : IToDo) {
+// {
+//   "text": "4",
+//   "category": "TO_DO",
+//   "id": 1645352937524
+// }
+/*
+
+  1. array 안에 있는 object 의 index를 찾는다.
+  2. 수정하고자 하는 todo의 경로를 알아야함.
+
+*/
+// 배열의 index 지키면서 요소 바꾸기
+// const food = ["pizza", "mango", "kimchi", "kimbab"]
+// const front = ["pizza"]
+// const back = ["kimchi", "kimbab"]
+//const final = [...front, "감", ...back]
+//console.log([...food.slice(0, 1), "감", ...food.slice(1+1)])
+
+function ToDo({text, category, id} : IToDo) {
+  const setToDos = useSetRecoilState(toDoState)
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos(oldToDos => {
+      const targetIndex = oldToDos.findIndex(toDo => toDo.id === id)
+      // 누른놈의 id와 원래있던 id 가 같은걸 찾음.
+      const oldToDo = oldToDos[targetIndex]
+      const newToDo = {text, id, category: name as any}
+      return [...oldToDos.slice(0, targetIndex), newToDo, ...oldToDos.slice(targetIndex+1)]
+      /*
+      3개중 2번째의 버튼을 눌럿을때 1번째 배열요소는 남아있고 그 다음에 newToDo 를
+      넣어준다음에 나머지 배열요소를 합침. newToDo는 name 의 값이 바뀌기 때문에
+      버튼의 value 값이 바뀐다.
+      */
+    })
+  }
+
   return (
     <li>
       <span>{text}</span> 
-      <button>Done</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== Categories.DOING && (
+        <button name={Categories.DOING} onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== Categories.TO_DO && (
+        <button name={Categories.TO_DO} onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE} onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   ) 
 }
